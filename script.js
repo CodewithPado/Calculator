279f32afe941fbee1131ffd8bccbf7629a689031
code_with_pado <padraigmullane@gmail.com>
2024-04-20 22:55:55 -0400

    feat: add basic arithmetic functions and variables
    Description:
    - Added functions for addition, subtraction, multiplication, and
      division.
    - Initialized user input variables for tracking numbers and operators.

---
+++ script.js
@@ -0,0 +1,18 @@
+// Create basic arithmetic functions
+function add(num1, num2) {
+  return num1 + num2;
+}
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+// Create user input variables
+let number1 = "";
+let number2 = "";
+let operator = "";
