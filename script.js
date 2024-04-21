868e5e0642263aac676704098a706ba4a7c0a7d4
code_with_pado <padraigmullane@gmail.com>
2024-04-21 13:35:20 -0400

    feat: add operate function for arithmetic ops
    - Added operate function to handle arithmetic operations.
    - It uses a switch statement to execute the correct arithmetic function
      based on the operator input.

--- script.js
+++ script.js
@@ -15,4 +15,24 @@ function divide(num1, num2) {
 // Create user input variables
 let number1 = "";
 let number2 = "";
-let operator = "";
+let operator = null;
+
+// Create function operate that takes two numbers and an operator, then based on the operator it calls one of the arithmetic functions.
+function operate(num1, op, num2) {
+  switch (op) {
+    case "+":
+      return add(num1, num2);
+
+    case "-":
+      return subtract(num1, num2);
+
+    case "*":
+      return multiply(num1, num2);
+
+    case "/":
+      return divide(num1, num2);
+
+    default:
+      null;
+  }
+}
