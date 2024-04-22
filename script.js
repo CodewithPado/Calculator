ff492a20c1034fa42d49df57ba68bdabd4618824
code_with_pado <padraigmullane@gmail.com>
2024-04-22 14:34:12 -0400

    feat: Add function for number and decimal handling
    - Added appendNumberOrDecimal function to handle user input of
      numbers and decimals. The function manages multiple decimals and
      updates number1 or number2 accordingly based on the current operator
      state.
    - Additionally, it ensures the display does not overflow beyond
      the defined maximum length

--- script.js
+++ script.js
@@ -2,14 +2,39 @@
 let number1 = "";
 let number2 = "";
 let operator = null;
+const maxLength = 10;
 let display = document.querySelector("#display");
 
 /*
-Create function populateDisplay() to ensure the display text does not overflow.
+Function populateDisplay() ensures the display text does not overflow.
+1. Shorten the display text if its length exceeds maxLength.
 */
 function populateDisplay() {
-  if (display.textContent.length > 10) {
-    display.textContent = display.textContent.substring(0, 10);
+  if (display.textContent.length > maxLength) {
+    display.textContent = display.textContent.substring(0, maxLength);
+  }
+}
+
+/*
+Create function appendNumberOrDecimal(input) to handle number and decimal input.
+1. Prevent multiple decimals in the display.
+2. If operator is null, append input to number1 and update display, ensuring no overflow.
+3. Otherwise, append input to number2 and update display, checking its length.
+*/
+function appendNumberOrDecimal(input) {
+  if (input === "." && display.textContent.includes(".")) {
+    return;
+  }
+  if (operator === null) {
+    // Handle input for the first number
+    if (number1.length < maxLength) {
+      number1 += input;
+      display.textContent = number1;
+    }
+  } else if (number2.length < maxLength) {
+    // Handle input for the second number
+    number2 += input;
+    display.textContent = number2;
   }
 }
 
@@ -17,12 +42,15 @@ function populateDisplay() {
 function add(num1, num2) {
   return num1 + num2;
 }
+
 function subtract(num1, num2) {
   return num1 - num2;
 }
+
 function multiply(num1, num2) {
   return num1 * num2;
 }
+
 function divide(num1, num2) {
   return num1 / num2;
 }
@@ -32,18 +60,14 @@ function operate(num1, op, num2) {
   switch (op) {
     case "+":
       return add(num1, num2);
-
     case "-":
       return subtract(num1, num2);
-
     case "*":
       return multiply(num1, num2);
-
     case "/":
       return divide(num1, num2);
-
     default:
-      null;
+      return null;
   }
 }
 
